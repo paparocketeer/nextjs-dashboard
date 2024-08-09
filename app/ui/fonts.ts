1edcbd35ed95eccfd87ec194e027f109a82e579f
paparocketeer <paparocketeer@gmail.com>
2024-08-09 20:04:16 +0300

    initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Nunito, Lusitana } from 'next/font/google';
+ 
+export const nunito = Nunito({ subsets: ['latin', 'cyrillic'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
