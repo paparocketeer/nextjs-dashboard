d1eadf1a42b345682f1b17e90a42bc53cf0bb1ab
paparocketeer <paparocketeer@gmail.com>
2024-08-10 13:19:43 +0300

    tail admin added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,47 @@
-import '@/app/ui/global.css';
-import { nunito } from '@/app/ui/fonts';
+// import '@/app/ui/global.css';
+// import { nunito } from '@/app/ui/fonts';
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${nunito.className} antialiased`}>{children}</body>
+//     </html>
+//   );
+// }
+
+"use client";
+import "jsvectormap/dist/jsvectormap.css";
+import "flatpickr/dist/flatpickr.min.css";
+import "@/css/satoshi.css";
+import "@/css/style.css";
+import React, { useEffect, useState } from "react";
+import Loader from "@/components/common/Loader";
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // const pathname = usePathname();
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
   return (
     <html lang="en">
-      <body className={`${nunito.className} antialiased`}>{children}</body>
+      <body suppressHydrationWarning={true}>
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? <Loader /> : children}
+        </div>
+      </body>
     </html>
   );
 }
